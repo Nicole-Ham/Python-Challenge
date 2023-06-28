7e76d5524afe6ee46f086517b567d785c69440d7
Nicole-Ham <NLHammack@gmail.com>
2023-06-28 14:33:50 -0700

    Fourth Commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,10 +8,40 @@ with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
+    dictionary ={} 
+    money = 0
     rowcounter = 0
-# want to keep in with statement so python knows to keep using within the open file'
+    previous_value = 0
+# want to keep inside with statement so python knows to keep using within the open file'
     for row in csvreader:
-        print(row[0])
         rowcounter = rowcounter + 1
+        money = money + int(row[1])
 
-print(rowcounter)
+        if rowcounter != 1:
+            profitloss = int(row[1]) - previous_value
+            dictionary[row[0]] = profitloss
+        previous_value = int(row[1])
+
+print()
+print("Financial Anaylsis")
+print()
+print("---------------------------------")
+print()
+print(f"Total Months: {rowcounter}")
+print(f"Total: ${money:,}")
+print(f"Average Change: ${sum(dictionary.values())/(rowcounter-1):.2f}") 
+# -1 one because we dont want to include the first line cause its where it started
+
+months_list = list(dictionary.keys())
+profit_loss_list = list(dictionary.values())
+lowest_value = min(profit_loss_list)
+lowest_index = profit_loss_list.index(lowest_value)
+lowest_month = months_list[lowest_index]
+highest_value = max(profit_loss_list)
+highest_index = profit_loss_list.index(highest_value)
+highest_month = months_list[highest_index]
+
+print(f"Greatest Increase in Profits: {highest_month} $({highest_value:,})")
+print(f"Greatest Decrease in Profits: {lowest_month}  $({lowest_value:,})")
+print()
+
